cf38ff2ca991db4ecb035475a542afeb1151af28
Tetraloops <142662049+Tetraloops@users.noreply.github.com>
2025-02-12 18:05:49 +0500

    Update models.py

--- project/hm/models.py
+++ project/hm/models.py
@@ -70,7 +70,9 @@ class evt_event_schedule(models.Model):
     event_order_id = models.ForeignKey(hm_event_order, on_delete=models.CASCADE)
     schedule_date = models.DateField()
     schedule_time = models.CharField(max_length=10)
-    entertainment = models.CharField(max_length=30)    #From Lookup Table
+    inv_category_id = models.ForeignKey('inv.inv_category', on_delete=models.CASCADE)    #Example word ENTERTAINMENT
+    inv_category_set_id = models.ForeignKey('inv.inv_category_sets', on_delete=models.CASCADE)    #Entered by user or select default from fnd_entertainment_type Table
+    inventory_id = models.ForeignKey('inv.inv_inventory', on_delete=models.CASCADE)    #Entered by user or select default from fnd_entertainments Table
     entertainment_rate = models.IntegerField()
     inventory_packing_id = models.ForeignKey('inv.inv_inventory_packing', on_delete=models.CASCADE)
     created_by = models.CharField(max_length=30)
